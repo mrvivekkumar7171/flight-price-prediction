a2bb2bb30e44d7fe997eb69a69b5767fcfd0549b
mrvivekkumar7171 <123541635+mrvivekkumar7171@users.noreply.github.com>
2025-03-19 15:35:06 +0530

    initial invalid input solved in predict.html

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import (
     render_template
 )
 from forms import InputForm
+from flask import request
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret_key"
@@ -17,27 +18,36 @@ model = joblib.load("model.joblib")
 def home():
     return render_template("home.html", title="Home")
 
-
 @app.route("/predict", methods=["GET", "POST"])
 def predict():
     form = InputForm()
-    if form.validate_on_submit():
-        x_new = pd.DataFrame(dict(
-            airline=[form.airline.data],
-            date_of_journey=[form.date_of_journey.data.strftime("%Y-%m-%d")],
-            source=[form.source.data],
-            destination=[form.destination.data],
-            dep_time=[form.dep_time.data.strftime("%H:%M:%S")],
-            arrival_time=[form.arrival_time.data.strftime("%H:%M:%S")],
-            duration=[form.duration.data],
-            total_stops=[form.total_stops.data],
-            additional_info=[form.additional_info.data]
-        ))
-        prediction = model.predict(x_new)[0]
-        message = f"The predicted price is {prediction:,.0f} INR!"
-    else:
-        message = "Please provide valid input details!"
-    return render_template("predict.html", title="Predict", form=form, output=message)
+    message = ""
+    first_submit = False
+    
+    if request.method == "POST":
+        first_submit = True  # Mark that form was submitted
+        
+        if form.validate_on_submit():
+            x_new = pd.DataFrame(dict(
+                airline=[form.airline.data],
+                date_of_journey=[form.date_of_journey.data.strftime("%Y-%m-%d")],
+                source=[form.source.data],
+                destination=[form.destination.data],
+                dep_time=[form.dep_time.data.strftime("%H:%M:%S")],
+                arrival_time=[form.arrival_time.data.strftime("%H:%M:%S")],
+                duration=[form.duration.data],
+                total_stops=[form.total_stops.data],
+                additional_info=[form.additional_info.data]
+            ))
+
+            # Predict
+            prediction = model.predict(x_new)[0]
+            message = f"The predicted price is {prediction:,.0f} INR!"
+        else:
+            message = "Please provide valid input details!"
+    
+    return render_template("predict.html", title="Predict", form=form, output=(message, first_submit))
+
 
 
 if __name__ == "__main__":
